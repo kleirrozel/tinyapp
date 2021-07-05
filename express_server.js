89c62cdb35d91a81c9759480d7b318a50c5038e3
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-05 15:29:44 -0400

    Added require body-parser to file

--- express_server.js
+++ express_server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+const app = express();
+
+const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -41,4 +46,3 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
 });
-
