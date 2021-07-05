2c6b20aabe6e814c4589bad560876946fe613fa1
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-05 15:56:57 -0400

    Added function that generates a random 6 character string

--- express_server.js
+++ express_server.js
@@ -46,3 +46,13 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  return Math.random().toString(20).substr(2, 6)
+};
+// console.log(generateRandomString());
