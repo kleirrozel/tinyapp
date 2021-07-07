0e1b54be1cf5160df01c4ee0e340c000fd1e1cda
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-06 23:32:24 -0400

    Cleaned up code and added comments

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = 8080;
+
+app.set("view engine", "ejs");
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,44 +19,27 @@ const generateRandomString = () => {
   return Math.random().toString(36).substr(2, 6);
 };
 
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-app.set("view engine", "ejs");
-
 // Shows up on terminal when the server is started
 app.listen(PORT, () => {
   console.log(`TinyApp is active on port ${PORT}!`);
 });
 
-
-// GET
-
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Welcome to TinyApp!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 // Route for urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase,
-    username: req.cookies["username"] };
-  
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"] 
+  };
   res.render("urls_index", templateVars);
 });
-
 
 // Route to the form that creates new urls
 // Note: Needs to be defined before /urls/:id
@@ -58,7 +47,6 @@ app.get("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies["username"] }; 
   res.render("urls_new", templateVars);
 });
-
 
 // Displays a single URL and its shortened form
 app.get("/urls/:shortURL", (req, res) => {
@@ -69,16 +57,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
 // Redirects to longURL
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL; 
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
-
-
-// POST
 
 // Once a form is submitted from GET /urls/new a request will be made to POST /urls
 // Saves the shortURL-longURL key-value pair to the urlDatabase
@@ -89,20 +73,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-
-// EDGE CASES:
-// What would happen if a client requests a non-existent shortURL?
-// What happens to the urlDatabase when the server is restarted?
-// What type of status code do our redirects have? What does this status code mean?
-
-
 // Adds a delete button and redirects back to /urls page
 app.post("/urls/:shortURL/delete", (req, res) => { 
   const shortURL = req.params.shortURL; 
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
-
 
 // Adds an edit button and redirects back to /urls page
 app.post("/urls/:id", (req, res) => { 
@@ -111,8 +87,7 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-
-// Logs in and allows usernames to be remembered
+// Logs in and allows usernames to be remembered through cookies
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
